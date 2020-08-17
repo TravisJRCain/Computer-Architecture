e0d05d492359462e8434dd925a646e6b24946367
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-08-17 16:06:50 -0700

    Almost done w/ module 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,28 +7,43 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 32
+        self.reg = [0] * 8
+        self.pc = 0 
+        self.reg = 0xF4
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
-
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        if filename:
+            with open(filename) as f:
+                address = 0
+            for line in f:
+                value = line.split('#')[0].strip()
+                if value == "":
+                    continue
 
-        for instruction in program:
+                else:
+                    instruction= int(value, 2)
+                    self.ram[address] = instruction
+                    address += 1
+
+        else:
+
+            program = [
+                # From print8.ls8
+                0b10000010, # LDI R0,8
+                0b00000000,
+                0b00001000,
+                0b01000111, # PRN R0
+                0b00000000,
+                0b00000001, # HLT
+            ]
+
+        for address, instruction in enumerate(program):
             self.ram[address] = instruction
-            address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -36,9 +51,22 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+        def ram_read(self, address):
+            return self.ram[address]
+
+        def ram_write(self, value, address):
+            self.ram[address] = value
 
     def trace(self):
         """
@@ -58,8 +86,7 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
-        print()
-
     def run(self):
         """Run the CPU."""
-        pass
+        
+        
